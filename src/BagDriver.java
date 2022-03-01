16650a9ae0af6b319054ee408e62c1a06dea42a0
amehr2003 <mehrazarin.a@gmail.com>
2022-02-28 21:36:59 -0800

    Attempted to fix test methods for errors.

--- src/BagDriver.java
+++ src/BagDriver.java
@@ -1,8 +1,9 @@
-/*
 package src;
 
 
 import org.w3c.dom.Node;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BagDriver {
     public static void main(String[] args)
@@ -10,23 +11,39 @@ public class BagDriver {
         /**
          * implementation example for resizeableArrayBag
          * implements methods union, difference, and intersection
+        */
 
-        ResizeableArrayBag<Integer> ArrayBag1 = new ResizeableArrayBag<>(2);
-        ArrayBag1.add(3);
-        ArrayBag1.add(7);
-        ResizeableArrayBag<Integer> ArrayBag2 = new ResizeableArrayBag<>(2);
-        ArrayBag2.add(8);
-        ArrayBag2.add(9);
+        ResizeableArrayBag<Integer> testUnionArrayBag1 = new ResizeableArrayBag<>(2);
+        testUnionArrayBag1.add(6);
+        testUnionArrayBag1.add(7);
+        ResizeableArrayBag<Integer> testUnionArrayBag2 = new ResizeableArrayBag<>(2);
+        testUnionArrayBag2.add(8);
+        testUnionArrayBag2.add(9);
 
-        BagInterface<Integer> union = intArrayBag.Union(intArrayBag2);
-        BagInterface<Integer> difference = intArrayBag.Difference(intArrayBag2);
-        BagInterface<Integer> intersection = intArrayBag.Intersection(intArrayBag2);
+        BagInterface<Integer> togethermain = testUnionArrayBag1.Union(testUnionArrayBag2);
+
+        System.out.println(togethermain);
+
+        /**
+         * showcasing intersection method for ResizeableArrayBag
+         */
+        ResizeableArrayBag<Integer> testIntersectionArrayBag1 = new ResizeableArrayBag<>(2);
+        testIntersectionArrayBag1.add(6);
+        testIntersectionArrayBag1.add(7);
+        ResizeableArrayBag<Integer> testIntersectionArrayBag2 = new ResizeableArrayBag<>(2);
+        testIntersectionArrayBag2.add(6);
+        testIntersectionArrayBag2.add(9);
+
+        BagInterface<Integer> testcommon = testIntersectionArrayBag1.Intersection(testIntersectionArrayBag2);
+
+        System.out.println(togethermain);
+
 
         /**
          * implementation example for linkedBag
          * implements methods union, difference, and intersection
-
-
+        */
+/*
         Node<Object> headNode = new Node(3, null);
         LinkedBag<Object> intLinkedBag = new LinkedBag<Object>(headNode);
         intLinkedBag.add(4);
@@ -38,9 +55,8 @@ public class BagDriver {
         BagInterface<Integer> union2 = intLinkedBag.Union(intLinkedBag2);
         BagInterface<Integer> difference2 = intLinkedBag.Difference(intLinkedBag2);
         BagInterface<Integer> intersection2 = intLinkedBag.Intersection(intLinkedBag2);
-
+*/
 
     }
 
 }
-*/
--- src/test/LinkedBagTest.java
+++ src/test/LinkedBagTest.java
@@ -1,4 +1,3 @@
-/*
 package src.test;
 
 import org.junit.jupiter.api.Test;
@@ -12,11 +11,13 @@ import static org.junit.jupiter.api.Assertions.*;
  * this class was created to use the maven dependency and
  * create j-unit test files to run code for errors.
  * The Bag uses an implementation of Integer in this class.
+ */
 
 class LinkedBagTest<Object> {
 
     /**
      * testUnion tests the method that should combine two
+    */
 
     @Test
     void testUnion() {
@@ -81,4 +82,3 @@ class LinkedBagTest<Object> {
         assertFalse(leftover.contains(5));
     }
 }
-*/
